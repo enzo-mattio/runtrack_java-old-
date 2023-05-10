df564b391352481eb72a1bc78ed37c7344fa0254
EnzoMattio <113895326+EnzoMattio@users.noreply.github.com>
2023-05-10 11:53:45 +0200

    [ADD] Job 11

---
+++ Jour02/Triangle.java
@@ -0,0 +1,16 @@
+package Jour02;
+
+import java.util.Scanner;
+
+public class Triangle {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Entrez un nombre :");
+    int num = sc.nextInt();
+    for (int i = 0; i < num; i++) {
+      
+      System.out.println("*".repeat(i+1));
+    
+  }
+}
+}
