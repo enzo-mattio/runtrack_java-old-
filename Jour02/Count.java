5236f5f531c05b03ff053096f773293b3926082d
EnzoMattio <113895326+EnzoMattio@users.noreply.github.com>
2023-05-10 10:32:31 +0200

    [CHANGE] First int

--- Jour02/Count.java
+++ Jour02/Count.java
@@ -1,7 +1,7 @@
 package Jour02;
 public class Count {
   public static void main(String[] args) {
-    for (int i = 0; i <= 100; i++) {
+    for (int i = 1; i <= 100; i++) {
       System.out.println(i);
     }
   }
