9c94c6747f227916a3be9b084eb1b3eaebc2bd1b
EnzoMattio <113895326+EnzoMattio@users.noreply.github.com>
2023-05-11 10:04:31 +0200

    [ADD] Job 1

---
+++ Jour03/Tab.java
@@ -0,0 +1,10 @@
+package Jour03;
+
+public class Tab {
+  public static void main(String[] args){
+    int[] myTable = {12,6,76,89};
+    for(int i : myTable){
+      System.out.println(i);
+    }
+  }
+}
