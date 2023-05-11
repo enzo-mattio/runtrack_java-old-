3bba846a43e2f2848a1a40602a73daa0fd9a6e9b
EnzoMattio <113895326+EnzoMattio@users.noreply.github.com>
2023-05-11 10:51:47 +0200

    [ADD] Job 7

---
+++ Jour03/Matrice.java
@@ -0,0 +1,15 @@
+package Jour03;
+
+public class Matrice {
+  public static void main(String[] args){
+  int[][] matrice1 = {{1,2,3},{4,5,6},{7,8,9}};
+  int[][] matrice2 = {{10,20,30},{40,50,60},{70,80,90}};
+  for(int i = 0; i < matrice1.length; i++){
+    for(int j = 0; j < matrice1[i].length; j++){
+      System.out.println("Valeur de la première matrice a l'index " +i +", "+ j +" : " + matrice1[i][j]);
+      System.out.println("Somme des deux valeurs a l'index " +i + ", "+ j + " additionné: " + (matrice1[i][j]+ matrice2[i][j]));
+    }
+  }
+
+}
+}
