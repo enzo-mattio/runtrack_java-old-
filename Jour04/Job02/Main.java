99acb9bf82a603f5407ef75ffcc8507b9da5874d
EnzoMattio <113895326+EnzoMattio@users.noreply.github.com>
2023-05-12 13:13:16 +0200

    [REMOVE] 3 threads

--- Jour04/Job02/Main.java
+++ Jour04/Job02/Main.java
@@ -25,22 +25,17 @@ public class Main extends Thread{
         }
         long startTime = System.nanoTime();
         //déclaration de 4 threads
-        Thread thread1 = new Thread(new Random(length/4 + length%4));
-        Thread thread2 = new Thread(new Random(length/4));
-        Thread thread3 = new Thread(new Random(length/4));
-        Thread thread4 = new Thread(new Random(length/4));
+        Thread thread1 = new Thread(new Random(length));
+        
         //démarrage des threads
         thread1.start();
-        thread2.start();
-        thread3.start();
-        thread4.start();
+        
         //attente de la fin des threads
 
         try {
             thread1.join();
-            thread2.join();
-            thread3.join();
-            thread4.join();
+            
+            
         } catch (InterruptedException e){
             e.printStackTrace();
         }
